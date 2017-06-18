4208feec7f52674584ae1e0e0f5e921364d9b0a0
David Le <david@walkerandcobrands.com>
2017-06-18 15:57:05 -0400

    Used forked wav player, reset before playing so we can reuse player

--- wav_player.go
+++ wav_player.go
@@ -6,7 +6,7 @@ import (
 	"io"
 	"os"
 
-	"github.com/cryptix/wav"
+	"github.com/dl103/wav"
 	"github.com/gordonklaus/portaudio"
 )
 
@@ -22,12 +22,16 @@ func NewPlayer(wavPath string) (p Player) {
 }
 
 // Currently only plays from the default output device. Can change this later
-func (p *Player) PlaySound() {
+func (p *Player) Play() {
 	out := make([]int16, 1)
+	// TODO: get the sample rate from the file
 	stream, err := portaudio.OpenDefaultStream(0, 1, 48000, len(out), &out)
 	chk(err)
 	defer stream.Close()
 	chk(stream.Start())
+
+	// Reset before playing so we can re-use the player
+	p.wavReader.Reset()
 
 	buf := new(bytes.Buffer)
 readLoop:
